0a6d0eaae15e29a3c3883fca7cd2668d87b1b4d8
sunny960 <sunnypardhan80@gmail.com>
2021-07-30 16:54:12 +0530

    aading log

--- orchestrator/test_runner/src/happy_path_v2.rs
+++ orchestrator/test_runner/src/happy_path_v2.rs
@@ -203,7 +203,7 @@ pub async fn deploy_cosmos_representing_erc20_and_check_adoption(
         if let Ok(res) = res {
             let erc20 = res.into_inner().erc20;
             info!(
-                "Successfully adopted {:?} token contract of {:?}",
+                "Successfully adopted {} token contract of {}",
                 token_to_send_to_eth, erc20
             );
             erc20_contract = Some(erc20);
@@ -211,7 +211,7 @@ pub async fn deploy_cosmos_representing_erc20_and_check_adoption(
         }
         delay_for(Duration::from_secs(1)).await;
         info!(
-            "logs after delay_for for 1 sec and TOTAL_TIMEOUT {} and start {}",
+            "logs after delay_for for 1 sec and TOTAL_TIMEOUT {:?} and start {:?}",
             TOTAL_TIMEOUT, start
         );
     }
